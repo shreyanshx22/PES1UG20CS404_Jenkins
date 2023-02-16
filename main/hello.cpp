369992a01bd5de4aaa6bc630eecabc23186529c2
Shreyansh Srivastava <88975311+shreyanshx22@users.noreply.github.com>
2023-02-16 12:05:18 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<<"My Name:Shreyansh Srivastava ; SRN:PES1UG20CS404\n"
   cout << "I have successfully built and run\n";
   return 0;
 }
